08057a4ee30ee97637ee497e04b6e2345bb63a53
simonpjflor <spatuesta@gmail.com>
2025-01-09 15:11:21 -0500

    cofi/win//extends product

--- projects/ShoppingMenu/src/ShoppingMenu/wine.java
+++ projects/ShoppingMenu/src/ShoppingMenu/wine.java
@@ -1,23 +1,7 @@
 package ShoppingMenu;
 
-public final class wine {
-    private final String name ="vino";
-    private final String description ="Vino tinto dulce";
-    private final double price =101;
-    private int stock=12;
-
-    @Override
-    public String toString() {
-        return "Producto: "+ name+
-                "Descripcion: "+ description+
-                "Precio: "+ price+
-                "Cantidad disponible: "+ stock;
-    }
-    public void setStock(int amount){
-        if (stock>0){
-            stock-=amount;
-        }else{
-            System.out.println();
-        }
-    }
+public final class wine extends Product{
+   public wine(){
+    super("vino","Vino tinto dulce",101.0,12);
+   }
 }
